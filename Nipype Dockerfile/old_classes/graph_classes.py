70b8a465121890f1dbe0b67ceef7225bcc638953
jniestroy <jniestroy@gmail.com>
2020-06-30 16:05:51 -0400

    Deprecated Old Code

---
+++ Nipype Dockerfile/old_classes/graph_classes.py
@@ -0,0 +1,193 @@
+import os
+import requests
+import json
+ORS_URL = os.environ.get("ORS_URL","http://mds.ors/")
+JOBID = os.environ.get("JOBID","testestest")
+class Output:
+
+    def __init__(self, name, location,node):
+
+        self.name = name
+        self.node = node
+        self.inputs = node.ids
+        self.location = location
+        self.id = None
+
+
+    def mint_and_upload(self):
+
+        computation_meta = {
+            "@type":"eg:Computation",
+            "name":"Computation " + str(self.name),
+            'eg:usedDataset':[],
+            'eg:usedSoftware':self.node.id
+        }
+        for input in self.inputs:
+            computation_meta['eg:usedDataset'].append({'@id':input})
+        comp_id = mint(computation_meta)
+
+        meta = {
+            'name':self.name,
+            "@type":"Dataset",
+            "eg:generatedBy":{'@id':comp_id},
+            "folder":JOBID
+        }
+
+        self.id = transfer(meta,self.location)
+
+        return self.id
+
+class Node:
+
+    def __init__(self, node):
+
+        self.node = node
+
+        input_dict = node.result.inputs
+        outputs = node.result.outputs
+
+        self.input_files, self.node_parameters = parse_inputs(input_dict)
+        self.parameters_id = mint(self.node_parameters)
+        self.output_files = gather_output_files(outputs)
+        self.ids = []
+
+        self.meta = {
+            "name":node.name,
+            "@type":"SoftwareSourceCode",
+            "interface":str(node.interface),
+            "parameters":self.parameters_id
+        }
+        self.id = mint(self.meta)
+
+
+
+def transfer(metadata,location):
+    files = {
+        'files':open(location,'rb'),
+        'metadata':json.dumps(metadata),
+    }
+    url = 'http://transfer-service/data/'
+    r = requests.post(url,files=files)
+    data_id = r.json()['Minted Identifiers'][0]
+    return data_id
+
+def mint(meta):
+    url = ORS_URL + "shoulder/ark:99999"
+
+    r = requests.post(url, data=json.dumps(meta))
+    returned = r.json()
+
+    if 'created' in returned:
+
+        return returned['created']
+    else:
+        return 'error'
+
+
+def gather_output_files(outputs):
+    sample = {}
+
+    for output in str(outputs).split('\n'):
+        #Go through all outputs
+        try:
+            name,result = output.split('=')
+            name = name.rstrip()
+            result = result.lstrip()
+            #if file is list parse into elements
+            if result[0] == '[' and result[-1] == ']':
+                result = result.strip('][').replace("'",'').split(', ')
+                #Can be list of ints so only keep track of files
+                files = []
+                for test in result:
+                    if not os.path.isfile(test):
+                        continue
+                    else:
+                        files.append(test)
+                result = files
+        except:
+            continue
+
+        if result == '<undefined>':
+            continue
+
+        sample[name] = result
+
+    return sample
+
+
+def parse_inputs(input_dict):
+    '''
+    Goal is to parse all inputs into a node. Return a dictionary of all inputs
+    that are files and dictionary of parameters.
+    e.g.
+    Returns something like
+    files = {
+        'input1':/path/to/a/location,
+        'input2':/second/path
+    }
+    parameters = {
+        'x':15,
+        'y':27,
+        'OUTPUTFILETYPE':'NIGZ'
+    }
+    '''
+
+    input_files = {}
+    parameters = {'@type':"CompuationParameters"}
+
+    for key in input_dict:
+        #Inputs that are dicts parse and add sub keys
+        if isinstance(input_dict[key],dict):
+            sub_inputs, sub_parameters = parse_inputs(input_dict[key])
+            for sub_key in sub_inputs:
+                input_files[key + '_' + sub_key] = sub_inputs[sub_key]
+            for sub_key in sub_parameters:
+                parameters[key + '_' + sub_key] = sub_parameters[sub_key]
+
+        elif isinstance(input_dict[key],list):
+            i = 0
+            for input in input_dict[key]:
+
+                if isinstance(input,dict):
+                    sub_inputs, sub_parameters = parse_inputs(input)
+                    for sub_key in sub_inputs:
+                        input_files[key + '_' + sub_key + '_' + str(i)] = sub_inputs[sub_key]
+                    for sub_key in sub_parameters:
+                        parameters[key + '_' + sub_key+ '_' + str(i)] = sub_parameters[sub_key]
+
+                elif isinstance(input,list):
+                    #Don't want to deal with lists of lists yet
+                    continue
+
+                elif isinstance(input,str) or isinstance(input,float) or isinstance(input,int):
+                    if isinstance(input,str):
+                        if os.path.isfile(input):
+                            input_files[key + '_' + str(i)] = input
+                    else:
+                        parameters[key + '_' + str(i)] = input
+                i = i + 1
+
+        elif isinstance(input_dict[key],str):
+            if os.path.isfile(input_dict[key]):
+                input_files[key] = input_dict[key]
+            else:
+                parameters[key] = input_dict[key]
+
+        else:
+            parameters[key] = input_dict[key]
+
+    return input_files,parameters
+
+# def get_node_inputs(wf,target):
+#
+#     adj = wf.__dict__['_adj']
+#
+#     for node in adj:
+#
+#         if node == target:
+#             continue
+#
+#         for sub_node in node:
+#
+#             if sub_node == target:
+#                 connection = sub_node[target]['connection']][0]
---
+++ Nipype Dockerfile/old_classes/old_parse.py
@@ -0,0 +1,53 @@
+ORS_URL = os.environ.get("ORS_URL","http://mds.ors/")
+def parse(wf):
+
+    with open('/meta/inputs.json') as json_file:
+        id_dict = json.load(json_file)
+
+    nodes = list(nx.topological_sort(wf))
+    for node in nodes:
+
+        #Using adjacency matrix find node outputs passed into
+        #current node
+        #connections = get_node_inputs(wf,node)
+
+        #converts Nipype node into simpler interpretation simply to parse
+        #inputs and outputs
+        current_node = Node(node)
+
+
+        for input_name,input in current_node.input_files.items():
+            try:
+                current_node.ids.append(id_dict[input])
+            except:
+                print('\n\n\n\n\n' + input + " unrecongized\n\n\n\n" )
+                continue
+
+        node_outputs = []
+        for output in current_node.output_files:
+
+            if isinstance(current_node.output_files[output],str):
+                #print(current_node.output_files[output])
+                file_path = current_node.output_files[output]
+                if file_path in id_dict.keys():
+                    continue
+                file_name = file_path.split('/')[-1]
+                node_outputs.append(Output(file_name,file_path,current_node))
+
+            else:
+
+                for file_path in current_node.output_files[output]:
+                    #print(file_path)
+                    if file_path in id_dict.keys():
+                        continue
+                    file_name = file_path.split('/')[-1]
+                    node_outputs.append(Output(file_name,file_path,current_node))
+
+
+        for output in node_outputs:
+            id_dict[output.location] = output.mint_and_upload()
+
+    with open('/meta/inputs.json', 'w') as outfile:
+        json.dump(id_dict, outfile)
+
+    return id_dict
